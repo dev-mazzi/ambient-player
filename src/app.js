ced4908e497d7426504865f55244e331e77e1949
dev-mazzi <v.maaazzi@gmail.com>
2022-10-03 00:36:22 +0900

    feat: Add Stop Watch

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-const player = document.getElementById('player');
+const time = document.querySelector(".time");
+
+let timeId;
+let currentTime = 0;
+let hour, min, sec;
+
+function printTime() {
+    currentTime++;
+    time.innerText = getTimeFormatString();
+    timeId = setTimeout(printTime, 1000);
+}
+
+function startTime() {
+    if (timeId == null)
+    timeId = setTimeout(printTime, 1000);
+}
+
+function stopTime() {
+    if (timeId != null) {
+        clearTimeout(timeId);
+        timeId = null;
+    }
+}
+
+function resetTime() {
+    stopTime();
+    time.innerText = "00:00:00";
+    currentTime = 0;
+}
+
+function getTimeFormatString() {
+    hour = parseInt(String(currentTime / (60 * 60)));
+    min = parseInt(String((currentTime - (hour * 60 * 60)) / 60));
+    sec = currentTime % 60;
+
+    return String(hour).padStart(2, '0') + ":" + String(min).padStart(2, '0') + ':' + String(sec).padStart(2, '0');
+}
